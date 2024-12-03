7c5d0388258e657dd8353c0ef008dc15d10020ec
Ben Aguirre <baguirre@luc.edu>
2024-12-02 23:41:39 -0600

    done day3

---
+++ day_3/part1.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const filePath = "./input.txt";
+const fileContent = fs.readFileSync(filePath, "utf8");
+
+const solve = (input) => {
+  const regex = /mul\(\d+,\d+\)/g;
+  let arr = [...input.matchAll(regex)].map((x) => x[0]);
+  let res = 0;
+  let doIt = true;
+  for (let line of arr) {
+    if (line == "do" || line == "don't") {
+      doIt = line == "do";
+      continue;
+    }
+    let curArr = line.split("");
+
+    let start = curArr.indexOf("(");
+    curArr = curArr.splice(start + 1);
+    curArr.pop();
+
+    let [num1, num2] = curArr.join("").split(",");
+    if (doIt) res += +num1 * +num2;
+  }
+
+  return res;
+};
+
+console.log(solve(fileContent));
