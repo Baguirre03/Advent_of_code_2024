e8be36dac41e65edc4ad4171f0aa7e8fd63ad616
Ben Aguirre <baguirre@luc.edu>
2024-12-04 13:37:46 -0600

    part 1 done

--- day_4/part1.js
+++ day_4/part1.js
@@ -2,40 +2,38 @@ const fs = require("fs");
 const filePath = "./input.txt";
 const fileContent = fs.readFileSync(filePath, "utf8").split("\n");
 
-const directions = [
-  [-1, 0],
-  [1, 0],
-  [0, 1],
-  [0, -1],
-  [-1, -1],
-  [-1, 1],
-  [1, -1],
-  [1, 1],
-];
+const directions = {
+  L: [-1, 0],
+  R: [1, 0],
+  D: [0, 1],
+  U: [0, -1],
+  UL: [-1, -1],
+  UR: [-1, 1],
+  DL: [1, -1],
+  DR: [1, 1],
+};
 
 const matrix = fileContent.map((x) => x.split(""));
 const xmas = ["X", "M", "A", "S"];
-const visited = new Set();
-const addVisit = (r, c, indx) => visited.add([r, c, indx].toString());
-const checkVisit = (r, c, indx) => visited.has([r, c, indx].toString());
 
-const outOfBounds = (r, c, indx) =>
+const outOfBounds = (r, c, indx, dir) =>
   r < 0 ||
   c < 0 ||
   r >= matrix.length ||
   c >= matrix[0].length ||
-  matrix[r][c] != xmas[indx] ||
-  checkVisit(r, c, indx);
+  matrix[r][c] !== xmas[indx];
 
 let res = 0;
-
 const solve = () => {
   let ROWS = matrix.length;
   let COLS = matrix[0].length;
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
-      if (matrix[r][c] == "X") {
-        dfs(r, c, 0);
+      if (matrix[r][c] == xmas[0]) {
+        const dirs = [...Object.keys(directions)];
+        dirs.forEach((d) => {
+          dfs(r, c, 0, d);
+        });
       }
     }
   }
@@ -43,16 +41,16 @@ const solve = () => {
   return res;
 };
 
-function dfs(r, c, indx) {
-  if (outOfBounds(r, c, indx)) return;
-  addVisit(r, c, indx);
-  if (indx == 3) {
-    res++;
-  }
+function dfs(r, c, indx, direction) {
+  if (outOfBounds(r, c, indx, direction)) return;
 
-  for (let [x, y] of directions) {
-    dfs(x + r, c + y, indx + 1);
+  if (indx == 3) {
+    res += 1;
   }
+  let [x, y] = directions[direction];
+  dfs(r + x, c + y, indx + 1, direction);
+
+  return;
 }
 
 console.log(solve(matrix));
